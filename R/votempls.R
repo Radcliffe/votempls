3196460057748a0949d7d00b8e606762cd809db2
David Radcliffe <dradcliffe@gmail.com>
2017-12-03 17:23:34 -0600

    Update documentation

--- R/votempls.R
+++ R/votempls.R
@@ -4,6 +4,8 @@
 #' Minneapolis elections. It includes the elections for mayor, city council,
 #' park and recreation commissioner, park board, and board of estimate and taxation.
 #'
+#' @section Getting Started:
+#' Type \code{data("MplsRankedChoice")} to load the data.
 #'
 #' @docType package
 #' @name votempls
