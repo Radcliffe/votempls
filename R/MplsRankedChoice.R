96bb40af886fbba2062ebe7c9a6705965eeb4b90
David Radcliffe <dradcliffe@gmail.com>
2017-12-03 17:11:37 -0600

    First commit

---
+++ R/MplsRankedChoice.R
@@ -0,0 +1,17 @@
+#' @title Minneapolis Ranked Choice Voting Data
+#' @description This data set contains ranked choice ballots for the 2013 and 2017 Minneapolis municipal elections
+#' @format A data frame with 930145 rows and 9 variables:
+#' \describe{
+#'   \item{Year}{election year (2013 or 2017)}
+#'   \item{City}{always equal to "Minneapolis"}
+#'   \item{Contest}{is one of the following: "Board of Estimate & Taxation",
+#'   "City Council", "Mayor", "Park & Recreation Commissioner", "Park Board"}
+#'   \item{PollingPlace}{contains the ward and precinct}
+#'   \item{Ward}{ward, numbered from 1 to 13}
+#'   \item{Precinct}{precinct}
+#'   \item{FirstChoice}{the voter's first choice}
+#'   \item{SecondChoice}{the voter's second choice}
+#'   \item{ThirdChoice}{the voter's third choice}
+#' }
+#' @source \url{http://vote.minneapolismn.gov/results/index.htm}
+"MplsRankedChoice"
---
+++ R/votempls.R
@@ -0,0 +1,10 @@
+#' votempls: Minneapolis ranked choice voting results.
+#'
+#' This dataset contains the ranked choice ballots for the 2013 and 2017
+#' Minneapolis elections. It includes the elections for mayor, city council,
+#' park and recreation commissioner, park board, and board of estimate and taxation.
+#'
+#'
+#' @docType package
+#' @name votempls
+NULL
